8a2313b52be9e68b6f80e609ffb78f805c831e3f
Yuki Suzuki <yukinko.iwasaki@gmail.com>
2026-04-01 13:16:47 -0400

    address about page

--- components/pefa.py
+++ components/pefa.py
@@ -203,7 +203,7 @@ def pefa_pillar_heatmap(df, lang="en"):
     heatmap_scores.sort_index(ascending=False, inplace=True)
     heatmap_grades.sort_index(ascending=False, inplace=True)
 
-    hover_text = heatmap_scores.applymap(lambda x: f"{x:.1f}" if not np.isnan(x) else "N/A")
+    hover_text = heatmap_scores.map(lambda x: f"{x:.1f}" if not np.isnan(x) else "N/A")
     hover_text = hover_text + "<br>Grade: " + heatmap_grades.values
 
     fig = go.Figure(
